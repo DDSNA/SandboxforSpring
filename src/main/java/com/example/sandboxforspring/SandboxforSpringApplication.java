85b69da58344ac6119589a4b2e427823c67bdf51
DDSNA <79444147+DDSNA@users.noreply.github.com>
2023-02-07 08:48:10 +0200

    Initial project setup. This one's a sandbox for spring in particular

---
+++ src/main/java/com/example/sandboxforspring/SandboxforSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.sandboxforspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxforSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandboxforSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sandboxforspring/SandboxforSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sandboxforspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxforSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
