878514da076c92312037086aa9d561e297345f2d
DDSNA <79444147+DDSNA@users.noreply.github.com>
2023-02-09 08:33:32 +0200

    Today I've learnt Jakarta and SE don't play well together

---
+++ src/test/java/com/example/sandboxforspring/WebServletTest.java
@@ -0,0 +1,16 @@
+package com.example.sandboxforspring;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WebServletTest {
+
+    @Test
+    void doGet() {
+    }
+
+    @Test
+    void doPost() {
+    }
+}
